60ece9610769d1bb030ca594a250de45ceee5434
Darrell007 <oddeidarrell36@gmail.com>
2025-06-14 22:58:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,9 @@ def run_signal():
 
 # === Run locally (ignored on Render) ===
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
